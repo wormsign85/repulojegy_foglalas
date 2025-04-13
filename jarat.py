e3b01b839d876fc37caf036d78a19921d891de15
wormsign85 <info@wormsign.hu>
2025-04-13 18:32:53 +0200

    Indulási időpont figyelés

--- jarat.py
+++ jarat.py
@@ -1,17 +1,18 @@
 from abc import ABC, abstractmethod
 
 class Jarat(ABC):
-    def __init__(self, jaratszam, celallomas, jegyar):
+    def __init__(self, jaratszam, celallomas, jegyar, indulas_idopont):
         self.jaratszam = jaratszam
         self.celallomas = celallomas
         self.jegyar = jegyar
+        self.indulas_idopont = indulas_idopont
 
     @abstractmethod
     def jarat_tipus(self):
         pass
 
     def __str__(self):
-        return f"{self.jarat_tipus()} - {self.jaratszam} ({self.celallomas}) | Ár: {self.jegyar} Ft"
+        return f"{self.jarat_tipus()} - {self.jaratszam} ({self.celallomas}) | Ár: {self.jegyar} Ft | Indulás: {self.indulas_idopont}"
 
 
 class BelfoldiJarat(Jarat):
